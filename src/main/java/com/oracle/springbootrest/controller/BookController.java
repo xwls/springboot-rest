f92b5462a1ef3ce3a8510df3238cc9f077dc5e52
xwls <351916362@qq.com>
2020-01-08 14:02:41 +0800

    完成添加、修改、删除

--- src/main/java/com/oracle/springbootrest/controller/BookController.java
+++ src/main/java/com/oracle/springbootrest/controller/BookController.java
@@ -1,12 +1,11 @@
 package com.oracle.springbootrest.controller;
 
+import com.baomidou.mybatisplus.extension.api.R;
 import com.oracle.springbootrest.entity.Book;
 import com.oracle.springbootrest.service.BookService;
 import com.oracle.springbootrest.util.Result;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,13 +16,35 @@ public class BookController {
     private BookService bookService;
 
     @GetMapping("/books")
-    public Result<List<Book>> books(){
+    public R<List<Book>> books(){
         List<Book> list = bookService.list();
-        Result<List<Book>> result = new Result<>();
-        result.setCode(0);
-        result.setMsg("操作成功");
-        result.setData(list);
-        return result;
+        return R.ok(list);
+    }
+
+//    @GetMapping("/book?bookId=102")
+    //使用路径传参
+    @GetMapping("/book/{bookId}")
+    public R<Book> book(@PathVariable Integer bookId){
+        Book book = bookService.getById(bookId);
+        return R.ok(book);
+    }
+
+    @PostMapping("/book")
+    public R<Book> save(Book book){
+        boolean save = bookService.save(book);
+        return save ? R.ok(book) : R.failed("添加失败");
+    }
+
+    @PutMapping("/book")
+    public R<Book> update(Book book){
+        boolean update = bookService.updateById(book);
+        return update ? R.ok(book) : R.failed("修改失败");
+    }
+
+    @DeleteMapping("/book/{bookId}")
+    public R<String> delete(@PathVariable Integer bookId){
+        boolean remove = bookService.removeById(bookId);
+        return remove ? R.ok("删除成功") : R.failed("删除失败");
     }
 
 }
