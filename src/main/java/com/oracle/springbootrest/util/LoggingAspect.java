a2a152ebe5213fb9fb9017f521527ad674da1149
xwls <351916362@qq.com>
2020-01-08 16:56:14 +0800

    获取方法细节及请求

--- src/main/java/com/oracle/springbootrest/util/LoggingAspect.java
+++ src/main/java/com/oracle/springbootrest/util/LoggingAspect.java
@@ -1,11 +1,18 @@
 package com.oracle.springbootrest.util;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 
 @Aspect
 @Component
@@ -13,16 +20,29 @@ public class LoggingAspect {
 
     private Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
-    //AOP的前置通知
+    //AOP的前置通知，在目标方法执行之前执行
     @Before("execution(public * com.oracle.springbootrest.controller.BookController.*(..))")
-    public void before(){
-        logger.debug("method begin.....");
+    public void before(JoinPoint joinPoint){
+        //获取方法名
+        String name = joinPoint.getSignature().getName();
+        logger.debug("method {} begin.....", name);
+        //获取方法参数
+        Object[] args = joinPoint.getArgs();
+        logger.debug("method args : {}.....", Arrays.toString(args));
+        //获取请求方法
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = requestAttributes.getRequest();
+        String method = request.getMethod();
+        //获取请求地址
+        String requestURI = request.getRequestURI();
+        logger.debug("{} => {}", method, requestURI);
     }
 
-    //AOP的后置通知
+    //AOP的后置通知，在目标方法执行之后执行
     @After("execution(public * com.oracle.springbootrest.controller.BookController.*(..))")
-    public void after(){
-        logger.debug("method end.....");
+    public void after(JoinPoint joinPoint){
+        String name = joinPoint.getSignature().getName();
+        logger.debug("method {} end.....", name);
     }
 
 }
